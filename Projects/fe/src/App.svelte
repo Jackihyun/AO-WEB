38efd62e46918ba5df2f6a134f4d014b42e062c3
Yoo-yongseok <hpsyong@gmail.com>
2024-02-06 03:12:39 +0900

    오류해결 된듯

--- Projects/fe/src/App.svelte
+++ Projects/fe/src/App.svelte
@@ -1,17 +1,5 @@
 <script>
-<<<<<<< HEAD
   import MainPage from "./pages/mainPage.svelte";
 </script>
 
 <MainPage/>
-=======
-  import svelteLogo from "./assets/svelte.svg";
-  import Mainpage from "./mainpage.svelte";
-  import Applypage from "./applypage.svelte";
-  import Apply from "./apply.svelte";
-</script>
-
-<Mainpage />
-<hr />
-<Apply />
->>>>>>> a4c61b6b0a8a241cf6e7377334be5825dd993bae
