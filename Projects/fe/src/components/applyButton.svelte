a4b5f5b60b6b6ee57daa6b2d36ee48eb1cf386d1
Jack <pkhjack2325@gmail.com>
2024-02-15 01:01:26 +0900

    scorllheader와 병합하고 메인페이지 버튼 색상 바뀌는 거 수정함

--- Projects/fe/src/components/applyButton.svelte
+++ Projects/fe/src/components/applyButton.svelte
@@ -3,7 +3,7 @@
   class="flex justify-center items-center w-[337px] mx-auto gap-2.5"
 >
   <div
-    class="active:bg-blue-700 active:scale-95 transition duration-150 ease-in-out flex flex-grow relative overflow-hidden gap-2.5 px-5 py-3.5 rounded-[50px] bg-[#191919]"
+    class="active:bg-blue-700 active:scale-95 transition duration-100 ease-in-out flex flex-grow relative overflow-hidden gap-2.5 px-5 py-3.5 rounded-[50px] bg-[#191919]"
   >
     <p
       class="flex-grow-0 flex-shrink-0 text-base text-center text-[#f2f2f2] mx-auto"
