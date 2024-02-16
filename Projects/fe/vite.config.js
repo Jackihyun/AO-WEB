3389dc05718cb26db4979a5b2a70a9186095fe4e
Jehee Cheon <jeheecheon@gmail.com>
2024-02-16 20:59:15 +0900

    applied proxy to fe

--- Projects/fe/vite.config.js
+++ Projects/fe/vite.config.js
@@ -5,4 +5,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      "/apply": {
+        target: "http://localhost:3000",
+        secure: false,
+        changeOrigin: true,
+      }
+    }
+  }
 })
